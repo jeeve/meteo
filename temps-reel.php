2498a001f31bcf1d7d07c54781054e2907e5915b
jeeve <jvjulien@free.fr>
2017-03-09 10:13:29 +0100

    raccorci

--- temps-reel.php
+++ temps-reel.php
@@ -1,9 +1,9 @@
 <?php	
 
 		$lines = file('http://www.weatherlink.com/user/troislacs/index.php?view=summary&amp;headers=1');
-		$vitesseVent = substr($lines[251], 39, 44);
-		$tempAir = substr($lines[143], 39, 44);
-		$tempEau = substr($lines[160], 39, 44); 
+		$vitesseVent = substr($lines[251], 39, 42);
+		$tempAir = substr($lines[143], 39, 42);
+		$tempEau = substr($lines[160], 39, 42); 
 
 		$arr = array('vitesseVent' => $vitesseVent, 'temperatureAir' => $tempAir, 'temperatureEau' => $tempEau);
 
