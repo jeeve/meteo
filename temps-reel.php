44e76246b55581ce2efe410b249748f2deace7ed
jeeve <jvjulien@free.fr>
2017-03-09 21:20:37 +0100

    250

--- temps-reel.php
+++ temps-reel.php
@@ -3,24 +3,24 @@
 		
 		$lines = file('http://www.weatherlink.com/user/troislacs/index.php?view=summary&amp;headers=1');
 
-		$line = substr($lines[251], 39, 3);
+		$line = substr($lines[250], 39, 3);
 		if ($line == "n/a")
 		{
 			$vitesseVent = "";
 		}
 		else
 		{
-			$vitesseVent = substr($lines[251], 39, 4);
+			$vitesseVent = substr($lines[250], 39, 4);
 		}
 
-		$line = substr($lines[143], 39, 3);
+		$line = substr($lines[142], 39, 3);
 		if ($line == "n/a")
 		{
 			$tempAir = "";
 		}
 		else
 		{
-			$tempAir = substr($lines[143], 39, 4);
+			$tempAir = substr($lines[142], 39, 4);
 		}
 				
 		$line = substr($lines[159], 39, 3);
