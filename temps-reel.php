cc6d791891f58f9141e02bd1d1ff6d79e8f65726
jeeve <jvjulien@free.fr>
2017-03-09 21:26:46 +0100

    parametre

--- temps-reel.php
+++ temps-reel.php
@@ -1,7 +1,7 @@
 <?php	
 		header("Access-Control-Allow-Origin: *");  
 		
-		$lines = file('http://www.weatherlink.com/user/troislacs/index.php?view=summary&amp;headers=1');
+		$lines = file('http://www.weatherlink.com/user/' . $_GET['station'] . '/index.php?view=summary&amp;headers=1');
 
 		$line = substr($lines[250], 39, 3);
 		if ($line == "n/a")
