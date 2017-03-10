0c7ef5676f3df340a32d7e6e86ea22df2dcaf705
jeeve <jvjulien@free.fr>
2017-03-10 10:29:59 +0100

    index

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
-<?php	
-
-		echo 'bonjour';
-?>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+	</head>
+	<body>
+		<p>
+		<a href="temps-reel.php">Web service météo https://meteo-station.herokuapp.com/temps-reel.php?station=[station]</a>
+		<p>
+		<a href="http://www.weatherlink.com/map.php">Liste des station</a>
+		</p>
+	</body>
+</html>	
