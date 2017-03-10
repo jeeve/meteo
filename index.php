b71e4a85f8137c94d78ad5df211dfb6aa3aec5eb
jeeve <jvjulien@free.fr>
2017-03-10 16:17:30 +0100

    index

--- index.php
+++ index.php
@@ -1,12 +1,64 @@
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-	</head>
-	<body>
-		<p>
-		Web service météo temps réel via weatherlink <a href="temps-reel.php">https://meteo-station.herokuapp.com/temps-reel.php?station=[station]</a>
-		<p>
-		<a href="http://www.weatherlink.com/map.php">Liste des stations</a>
-		</p>
-	</body>
-</html>	
+<?php	
+		header("Access-Control-Allow-Origin: *");
+
+		$station = 'troislacs';
+
+		if ($_GET['station'] != '') {
+				$station = $_GET['station'];
+		}	
+		
+		$lines = file('http://www.weatherlink.com/user/' . $station . '/index.php?view=summary&amp;headers=1');
+
+		$line = substr($lines[250], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);
+		if ($line == "n/a")
+		{
+			$vitesseVent = "";
+		}
+		else
+		{
+			$vitesseVent = $line;
+		}
+
+		$line = substr($lines[226], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);
+		if ($line == "n/a")
+		{
+			$orientationVent = "";
+		}
+		else
+		{
+			$orientationVent = $line;
+		}
+		
+		$line = substr($lines[142], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);
+		if ($line == "n/a")
+		{
+			$tempAir = "";
+		}
+		else
+		{
+			$tempAir = $line;
+		}
+				
+		$line = substr($lines[159], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);		
+		if ($line == "n/a")
+		{
+			$tempEau = "";
+		}
+		else
+		{
+			$tempEau = $line;
+		}
+
+		$arr = array('vitesseVent' => $vitesseVent, 'orientationVent' => $orientationVent, 'temperatureAir' => $tempAir, 'temperatureEau' => $tempEau);
+
+		echo json_encode($arr);
+?>
+	
