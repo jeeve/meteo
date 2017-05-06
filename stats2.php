d577cb8ea0a8728229f2ee4cebee1801d9d8a29e
jeeve <jvjulien@free.fr>
2017-05-06 18:40:14 +0200

    stat2

--- stats2.php
+++ stats2.php
@@ -4,7 +4,9 @@ header("Access-Control-Allow-Origin: *");
 $sUrl = 'http://mywindstats.com/windrose';
     $params = array('http' => array(
     'method' => 'POST',
-'content' => 'S_Spot=Lery-Poses&S_SelectedDate=2017-04-22&S_StartHour=13:00&S_EndHour=17:00&S_Unit=noeud'
+    'header' => 'Content-type: application/x-www-form-urlencoded\r\n'
+    . "Content-Length: " . strlen('S_Spot=Lery-Poses&S_SelectedDate=2017-04-22&S_StartHour=13&S_EndHour=17&S_Graph=Graphe+de+la+vitesse+du+vent&S_Unit=noeud') . "\r\n",
+'content' => 'S_Spot=Lery-Poses&S_SelectedDate=2017-04-22&S_StartHour=13&S_EndHour=17&S_Graph=Graphe+de+la+vitesse+du+vent&S_Unit=noeud'
 ));
 
 $ctx = stream_context_create($params);
@@ -22,6 +24,7 @@ if ($response === false)
 //throw new Exception("Problem reading data from $sUrl, $php_errormsg");
 }
 
+//echo stream_get_line($fp, 20);
 echo $response;
 echo "titi";
 ?>
