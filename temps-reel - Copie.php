035609ef01cf044fce540e6bafb63bcf111c48fe
jeeve <jvjulien@free.fr>
2018-10-25 23:04:01 +0200

    getline

--- temps-reel - Copie.php
+++ temps-reel - Copie.php
@@ -1,15 +1,6 @@
 <?php	
 		header("Access-Control-Allow-Origin: *");
 
-		function getLigne($tableau, $terme) {
-			for (i = 0; i < $tableau.length; i++) {
-				if (strpos($tableau[i], $terme) {
-					return i;
-				}
-			}
-			return false;
-		}
-		
 		$station = 'troislacs';
 
 		if ($_GET['station'] != '') {
@@ -18,8 +9,7 @@
 		
 		$lines = file('http://www.weatherlink.com/user/' . $station . '/index.php?view=summary&amp;headers=1');
 
-		//$line = substr($lines[250], 39, 20);
-		$ligne = substr($lines[getLigne($lines, "Average Wind Speed") + 1], 39, 20);
+		$line = substr($lines[250], 39, 20);
 		$p = strpos($line, '<');
 		$line = substr($line, 0, $p);
 		if ($line == "n/a")
