b693ab8a7cc90f1f5f5f16669a137835eb57d656
jeeve <jvjulien@free.fr>
2018-10-25 23:26:37 +0200

    line

--- temps-reel.php
+++ temps-reel.php
@@ -19,7 +19,7 @@
 		$lines = file('http://www.weatherlink.com/user/' . $station . '/index.php?view=summary&amp;headers=1');
 
 		//$line = substr($lines[250], 39, 20);
-		$ligne = substr($lines[getLigne($lines, "Average Wind Speed") + 2], 39, 20);
+		$line = substr($lines[getLigne($lines, "Average Wind Speed") + 2], 39, 20);
 		$p = strpos($line, '<');
 		$line = substr($line, 0, $p);
 		if ($line == "n/a")
