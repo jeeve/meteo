fcbeeab7f62035235bd50749f70cfb115750b188
jeeve <jvjulien@free.fr>
2018-11-27 23:30:15 +0100

    temps-reel-2

---
+++ temps-reel - Copie (2).php
@@ -0,0 +1,77 @@
+<?php	
+		header("Access-Control-Allow-Origin: *");
+
+		function getLigne($tableau, $terme) {
+			for ($i = 0; $i < count($tableau); $i++) {
+				if (strpos($tableau[$i], $terme)) {
+					return $i;
+				}
+			}
+			return false;
+		}
+		
+		$station = 'troislacs';
+
+		if ($_GET['station'] != '') {
+				$station = $_GET['station'];
+		}	
+		
+		$lines = file('http://www.weatherlink.com/user/' . $station . '/index.php?view=summary&amp;headers=1');
+
+		//$line = substr($lines[250], 39, 20);
+		$line = substr($lines[getLigne($lines, "Average Wind Speed") + 1], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);
+		if ($line == "n/a")
+		{
+			$vitesseVent = "";
+		}
+		else
+		{
+			$vitesseVent = $line;
+		}
+
+		//$line = substr($lines[225], 39, 20);
+		$line = substr($lines[getLigne($lines, "Wind Direction") + 1], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);
+		if ($line == "n/a")
+		{
+			$orientationVent = "";
+		}
+		else
+		{
+			$orientationVent = $line;
+		}
+		
+		//$line = substr($lines[141], 39, 20);
+		$line = substr($lines[getLigne($lines, "Outside Temp") + 1], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);
+		if ($line == "n/a")
+		{
+			$tempAir = "";
+		}
+		else
+		{
+			$tempAir = $line;
+		}
+				
+		//$line = substr($lines[303], 39, 20);
+		$line = substr($lines[getLigne($lines, "Soil Temp 2") + 1], 39, 20);
+		$p = strpos($line, '<');
+		$line = substr($line, 0, $p);		
+		if ($line == "n/a")
+		{
+			$tempEau = "";
+		}
+		else
+		{
+			$tempEau = $line;
+		}
+
+		$arr = array('station' => $station, 'vitesseVent' => $vitesseVent, 'orientationVent' => $orientationVent, 'temperatureExterieure' => $tempAir, 'temperatureInterieure' => $tempEau);
+
+		echo json_encode($arr);
+?>
+	
---
+++ temps-reel-2.php
@@ -0,0 +1,14 @@
+<?php	
+		header("Access-Control-Allow-Origin: *");
+	
+		$station = '33782285-df4e-4432-a3b3-06ddf1b3680a';
+
+		if ($_GET['station'] != '') {
+				$station = $_GET['station'];
+		}	
+		
+		$json = file_get_contents('https://www.weatherlink.com/map/data/station/' . $station);
+		
+		echo $json;
+?>
+	
