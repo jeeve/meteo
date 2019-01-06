3d78bd56189bd2afbcab812e5f51fa63ff34cc18
jeeve <jvjulien@free.fr>
2019-01-06 14:00:59 +0100

    param

--- map.php
+++ map.php
@@ -5,13 +5,10 @@
 <script>
 
 <?php 
-		$destination = 'poses';
-		if ($_GET['destination'] != '') {
-				$destination = $_GET['destination'];
+		$dest = '1s0x47e1289bac44658f%3A0xe940a5c757b0bf2a!2sL%C3%A9ry-Poses+en+Normandie%2C+CD+110%2C+27740+Poses!3m2!1d49.302783!2d1.209404!5e1!3m2!1sfr!2sfr!4v1546773316328';
+		if ($_GET['dest'] != '') {
+				$dest = $_GET['dest'];
 		}	
-		if ($destination == 'poses') {
-			$dest = '1s0x47e1289bac44658f%3A0xe940a5c757b0bf2a!2sL%C3%A9ry-Poses+en+Normandie%2C+CD+110%2C+27740+Poses!3m2!1d49.302783!2d1.209404!5e1!3m2!1sfr!2sfr!4v1546773316328';
-		}
 ?>
 		
 		
@@ -30,7 +27,7 @@
 
 </head>
 <body>
-<iframe id="map-poses" src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d353971.9888214055!2d2.2661361853305393!3d48.91901435220709!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d48.4061829!2d0.1372031!4m5!1s0x47e1289bac44658f%3A0xe940a5c757b0bf2a!2sL%C3%A9ry-Poses+en+Normandie%2C+CD+110%2C+27740+Poses!3m2!1d49.302783!2d1.209404!5e1!3m2!1sfr!2sfr!4v1546773316328" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<iframe id="map-poses" src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d353971.9888214055!2d2.35!3d48.853!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e0!4m3!3m2!1d48.4061829!2d0.1372031!4m5!1s0x47e1289bac44658f%3A0xe940a5c757b0bf2a!2sL%C3%A9ry-Poses+en+Normandie%2C+CD+110%2C+27740+Poses!3m2!1d49.302783!2d1.209404!5e1!3m2!1sfr!2sfr!4v1546773316328" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </body>	
 </html>
 		
