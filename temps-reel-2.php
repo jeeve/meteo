9e77319aed79e5191e4aaf9c7d15a77de8198b94
jeeve <jvjulien@free.fr>
2019-01-08 13:07:19 +0100

    test

--- temps-reel-2.php
+++ temps-reel-2.php
@@ -1,5 +1,14 @@
 <?php	
-		header("Access-Control-Allow-Origin: *");
+//		header("Access-Control-Allow-Origin: *");
+
+$origin = $_SERVER['HTTP_ORIGIN'];
+$allowed_domains = [
+    'https://meteo-station.herokuapp.com'
+];
+
+if (in_array($origin, $allowed_domains)) {
+    header('Access-Control-Allow-Origin: ' . $origin);
+}
 	
 		$station = '33782285-df4e-4432-a3b3-06ddf1b3680a';
 
