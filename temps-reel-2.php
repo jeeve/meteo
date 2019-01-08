0a6a6d44030ff22bfe3d44df8ae63ee664f504e0
jeeve <jvjulien@free.fr>
2019-01-08 14:11:56 +0100

    hh

--- temps-reel-2.php
+++ temps-reel-2.php
@@ -4,7 +4,7 @@
      function get_url($url) {
     	$curl = curl_init();
     	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json');
+		curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: application/json'));
 		curl_setopt($curl, CURLOPT_URL, $url);
     	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
     	curl_setopt($curl, CURLOPT_HEADER, true);
